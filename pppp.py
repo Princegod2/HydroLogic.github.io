8d1fcd3f272a3454c938c87ae0371e1b670f9461
Princegod2 <bhumika.rijal22@gmail.com>
2026-02-02 19:48:47 +0545

    Add files via upload

---
+++ pppp.py
@@ -0,0 +1,8 @@
+
+study_hours = int(input("How many hours will you study? "))
+
+water_needed = study_hours * 250
+breaks_needed = study_hours * 3
+
+print(f"You need to drink {water_needed}ml of water.")
+print(f"You should take {breaks_needed} eye breaks.")
